2e2f2f2ad6bbed3c5334afacc8a59732971eb14b
hiya-jindal <137200203+hiya-jindal@users.noreply.github.com>
2024-03-11 17:44:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX= 0; 
+
+function preload() {
+}
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,7 +19,11 @@ function setup() {
 }
 
 function draw() {
-    background('#969A97');
+    background('#F2B8C5');
+    fill('#000000');
+    stroke('#000000');
+    textSize(difference);
+    text('Hiya', noseX, noseY, difference);
 }
 
 function modelLoaded() {
@@ -22,5 +35,14 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;  
+        difference = floor(leftWristX - rightWristX); 
+        
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + "difference = " + difference); 
     }
 }
